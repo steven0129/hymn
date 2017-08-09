ba980f613e67d0647efb84710fec7d47b7d58dbc
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-08-09 23:45:03 +0800

    migration to element-ui

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,39 +1,37 @@
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: 'starter',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Nuxt.js project'
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
   },
   /*
-  ** Global CSS
-  */
+   ** Global CSS
+   */
   css: ['~/assets/css/main.css'],
   /*
-  ** Add axios globally
-  */
+   ** Add axios globally
+   */
   build: {
-    vendor: ['axios'],
-    /*
-    ** Run ESLINT on save
-    */
-    extend (config, ctx) {
-      if (ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
-  }
+    vendor: ['axios']
+  },
+  plugins: ['plugins/element-ui.js']
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,40 @@
 <template>
-  <section class="container">
-    <img src="~assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
-    <h1 class="title">
-      USERS
-    </h1>
-    <ul class="users">
-      <li v-for="(user, index) in users" :key="index" class="user">
-        <nuxt-link :to="{ name: 'id', params: { id: index }}">
-          {{ user.name }}
-        </nuxt-link>
-      </li>
-    </ul>
-  </section>
+  <div>
+    <el-row>
+      <el-col :span="24">
+        <el-button>Default Button</el-button>
+        <el-button type="primary">Primary Button</el-button>
+        <el-button type="text">Text Button</el-button>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24">
+        <el-input-number v-model="num1" :min="1" :max="10"></el-input-number>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
-import axios from '~/plugins/axios'
-
 export default {
-  async asyncData () {
-    let { data } = await axios.get('/api/users')
-    return { users: data }
-  },
-  head () {
+  layout: 'teub',
+  data() {
     return {
-      title: 'Users'
+      num1: 1,
+      color: 'rgba(19, 206, 102, 0.8)'
     }
   }
 }
 </script>
 
-<style scoped>
-.title
-{
-  margin: 30px 0;
+<style>
+body {
+  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", Arial, sans-serif;
+  padding-top: 50px;
 }
-.users
-{
-  list-style: none;
-  margin: 0;
-  padding: 0;
-}
-.user
-{
-  margin: 10px 0;
+
+.el-row {
+  padding: 10px;
+  text-align: center;
 }
 </style>
